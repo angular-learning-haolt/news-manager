23330dd26cc412ff80cc7f0b6e2c7c686fcf7094
122208997 <lehaohust@gmail.com>
2019-04-04 09:44:06 +0700

    switch to Linux use

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -8,23 +8,23 @@ import { map } from 'rxjs/operators';
 })
 export class NewsService {
 
-  public apiUrl = 'https://demo.crefox.com/news-sun-training/';
-  public errStatus: string;
+    public apiUrl = 'https://demo.crefox.com/news-sun-training/';
+    public errStatus: string;
 
-  constructor(
-       private http: HttpClient
-  ) { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  getAllNews(page: number, perPage: number) {
+    getAllNews(page: number, perPage: number) {
     return this.http.get<any>(
-      this.buildUrl('wp-json/wp/v2/posts'),
-      {
+        this.buildUrl('wp-json/wp/v2/posts'),
+        {
         params: this.buildParams({
             page,
             per_page: perPage
         }),
         observe: 'response'
-      }
+        }
     ).pipe(
         map((response) => {
             const data = response.body;
@@ -35,47 +35,62 @@ export class NewsService {
             };
         })
     );
-  }
+    }
 
-  getResponseHeader() {
-      return this.http.get(
+    getAllNewsStatus() {
+        return this.http.get<any>(
+            this.buildUrl('wp-json/wp/v2/statuses'),
+            // {
+            //     headers: this.buildHeader({
+            //         token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGVtby5jcmVmb3guY29tXC9uZXdzLXN1bi10cmFpbmluZyIsImlhdCI6MTU1NDM0MTA5MSwibmJmIjoxNTU0MzQxMDkxLCJleHAiOjE1NTQ5NDU4OTEsImRhdGEiOnsidXNlciI6eyJpZCI6IjUifX19.nij2sxdZYmTDQGz_BrtmOXmQLB3UbKgHpe_FZnwwMSY'
+            //     })
+            // }
+        )
+        // {
+        // params: this.buildParams({
+        // })
+        // })
+    };
+
+    getResponseHeader() {
+        return this.http.get(
         this.buildUrl('wp-json/wp/v2/posts'),
-      );
-  }
+        );
+    }
 
-  getNewsByID(id: number) {
+    getNewsByID(id: number) {
     return this.http.get<any>(
-      this.buildUrl('wp-json/wp/v2/posts/' + id),
+        this.buildUrl('wp-json/wp/v2/posts/' + id),
     //   { params: this.buildParams({
     //     id
     //   })}
     );
-  }
-
-  handleError(err) {
-    if (err.error instanceof Error) {
-      console.log(`Client side Error: ${ err.error.message }`);
-    } else {
-      this.errStatus = err.status;
-      console.log(this.errStatus);
     }
-  }
 
-  // build header for request
-  private buildHeader(): HttpHeaders {
+    handleError(err) {
+    if (err.error instanceof Error) {
+        console.log(`Client side Error: ${ err.error.message }`);
+    } else {
+        this.errStatus = err.status;
+        console.log(this.errStatus);
+    }
+    }
+
+    // build header for request
+    private buildHeader(): HttpHeaders {
     const header = new HttpHeaders({
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
     });
     return header;
-  }
+    }
 
-  private buildParams(paramsData): HttpParams {
+    private buildParams(paramsData): HttpParams {
     const params = new HttpParams({ fromObject: paramsData });
     return params;
-  }
-  // build url for request
-  private buildUrl(endpoint: string): string {
+    }
+    // build url for request
+    private buildUrl(endpoint: string): string {
     return this.apiUrl + endpoint;
-  }
+    }
 }
--- src/app/news/search-form/search-form.component.ts
+++ src/app/news/search-form/search-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 // import { Subscription } from 'rxjs/Subscription';
+import { NewsService } from './../news.service';
 
 @Component({
   selector: 'app-search-form',
@@ -10,11 +11,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SearchFormComponent implements OnInit, OnDestroy {
 
     public keywords = '';
+    public newsStatuses:any= [];
 
     constructor(
         private router: Router,
         private activatedRoute: ActivatedRoute,
         // private subscription: Subscription
+        private newsService: NewsService
     ) { }
 
     ngOnInit() {
@@ -25,7 +28,14 @@ export class SearchFormComponent implements OnInit, OnDestroy {
         this.activatedRoute.queryParams
             .subscribe(
                 data => this.keywords = data.keywords
-        )
+        );
+        this.newsService.getAllNewsStatus()
+            .subscribe(
+                data => {
+                    this.newsStatuses = data;
+                    console.log(this.newsStatuses);
+                }
+        );
     }
 
     // Input: (ngModelChange)="onChange()"
